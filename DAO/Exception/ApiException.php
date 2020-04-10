f49cfda36d8e9cc9237bad26c51e32dc7c47d470
maxime <maxime@zangra.com>
2020-04-10 12:12:29 +0200

    CleanUp wrong comment

--- DAO/Exception/ApiException.php
+++ DAO/Exception/ApiException.php
@@ -9,8 +9,6 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 /**
  * Class ApiException
  *
- * @package aibianchi\ExactOnlineBundle\DAO\Exception;
- *
  */
 class ApiException extends HttpException implements ApiExceptionInterface
 {
--- DAO/Exception/ApiExceptionInterface.php
+++ DAO/Exception/ApiExceptionInterface.php
@@ -5,8 +5,6 @@ namespace GeodisBundle\DAO\Exception;
 /**
  * interface ApiExceptionInterface
  *
- * @package aibianchi\ExactOnlineBundle\DAO\Exception;
- *
  */
 
 interface ApiExceptionInterface
